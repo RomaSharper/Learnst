febc6b31c3fabba4069fa24578d0414c9495ed1b
Roma Shibalov <shibalovrn@krista.ru>
2025-06-06 10:35:35 +0300

    The hierarchy have been updated, styles have been decomposed and repaired, image copy protection has been added

--- learnst-client/src/directives/no-downloading.directive.ts
+++ learnst-client/src/directives/no-downloading.directive.ts
@@ -1,8 +1,6 @@
 import {Directive, HostBinding} from '@angular/core';
 
-@Directive({
-  selector: '[noDownloading]'
-})
+@Directive({selector: '[noDownloading]'})
 export class NoDownloadingDirective {
   @HostBinding('attr.draggable') draggable = 'false';
   @HostBinding('attr.oncontextmenu') onContextMenu = 'return false;';
